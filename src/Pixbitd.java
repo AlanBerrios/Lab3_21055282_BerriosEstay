ad7cac25287b954e475ab008b7569dba0d759fd2
AlanBerrios <alan.berrios@usach.cl>
2022-11-19 22:35:06 -0300

    Create Pixbitd.java
    se crea la carpeta src y se agrega la clase pixbitd

---
+++ src/Pixbitd.java
@@ -0,0 +1,56 @@
+
+// Nombre de la clase
+
+public class Pixbitd {
+	
+	// Atributos de la clase
+	
+	private int y;
+	private int x;
+	private int bit;
+	private int depth;
+	
+	// Constructor 
+	
+	public Pixbitd(int newy, int newx, int newbit, int newdepth)
+	{
+		setY(newy);
+		setX(newx);
+		setBit(newbit);
+		setDepth(newdepth);
+		
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getBit() {
+		return bit;
+	}
+
+	public void setBit(int bit) {
+		this.bit = bit;
+	}
+
+	public int getDepth() {
+		return depth;
+	}
+
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+
+}
