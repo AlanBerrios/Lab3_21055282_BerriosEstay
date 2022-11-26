1867f356f9175c7a7092739c4093fe8c8f54438e
AlanBerrios <alan.berrios@usach.cl>
2022-11-25 22:42:33 -0300

    Create Image.java
    se crea el la clase tda image y su constructor

---
+++ src/tdas/Image.java
@@ -0,0 +1,25 @@
+package tdas;
+
+import java.util.List;
+
+public class Image{
+	
+	// Atributos
+	
+	int ancho;
+	int alto;
+	List<Pixel> listapixeles;
+	
+	// Metodos
+	
+	public Image(int ancho, int alto, List<Pixel> listapixeles) {
+	
+		this.ancho = ancho;
+		this.alto = alto;
+		this.listapixeles = listapixeles;
+	}
+	
+	
+
+
+	}
